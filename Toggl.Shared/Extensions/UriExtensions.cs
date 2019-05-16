054793ec10fdacc902199b7010b8f41960ab465b
Daivid S. Silverio <daivid.silverio@gmail.com>
2019-05-15 21:09:59 -0400

    :construction: Add extension to get query params

---
+++ Toggl.Shared/Extensions/UriExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using Toggl.Shared.Extensions.RxAction;
+
+namespace Toggl.Shared.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Dictionary<string, string> GetQueryParams(this Uri uri)
+            => HttpUtility
+                .ParseQueryString(uri.Query)
+                .ToDictionary(CommonFunctions.Identity, CommonFunctions.Identity);
+    }
+}
