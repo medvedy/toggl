58c3a16e0ced9705c69e1352fe2a72e94cf257ec
Daivid S. Silverio <daivid.silverio@gmail.com>
2019-05-20 19:38:38 -0400

    :construction: Use helper on url handler activity as well

--- Toggl.Droid/Startup/ExternalIntentsHandlerActivity.cs
+++ Toggl.Droid/Startup/ExternalIntentsHandlerActivity.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Reactive;
-using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Support.V7.App;
-using Toggl.Core.UI.Navigation;
 using Toggl.Core;
 using Toggl.Core.UI;
 using Toggl.Core.UI.Parameters;
 using Toggl.Core.UI.ViewModels;
-using Toggl.Core.UI.Views;
 using Toggl.Droid.BroadcastReceivers;
 using Toggl.Droid.Helper;
 using Toggl.Droid.Presentation;
@@ -34,7 +28,7 @@ namespace Toggl.Droid.Startup
         new[] { "android.intent.action.PROCESS_TEXT" },
         Categories = new[] { "android.intent.category.DEFAULT" },
         DataMimeType = "text/plain")]
-    public class ExternalIntentsHandlerActivity : AppCompatActivity, IView
+    public class ExternalIntentsHandlerActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -59,10 +53,8 @@ namespace Toggl.Droid.Startup
             
             if (string.IsNullOrEmpty(navigationUrl))
             {
-                AndroidDependencyContainer.Instance
-                    .NavigationService
-                    .Navigate<MainTabBarViewModel>(this)
-                    .ContinueWith(_ => Finish());
+                AndroidStartupHelper.StartMainTabBarActivity(this);
+                Finish();
                 return;
             }
 
@@ -82,32 +74,5 @@ namespace Toggl.Droid.Startup
             var applicationUrl = ApplicationUrls.Track.Default(description);
             return applicationUrl;
         }
-
-        public IObservable<bool> Confirm(string title, string message, string confirmButtonText, string dismissButtonText)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<Unit> Alert(string title, string message, string buttonTitle)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<bool> ConfirmDestructiveAction(ActionType type, params object[] formatArguments)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<T> Select<T>(string title, IEnumerable<SelectOption<T>> options, int initialSelectionIndex)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<bool> RequestCalendarAuthorization(bool force = false)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<bool> RequestNotificationAuthorization(bool force = false)
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public void OpenAppSettings()
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public IObservable<string> GetGoogleToken()
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
-
-        public Task Close()
-            => throw new InvalidOperationException("You shouldn't be doing this from a splash screen");
     }
 }
